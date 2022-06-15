a7588222ee156688abf32fc9247ca2b1d86740d9
Heyrin <77548431+devheyrin@users.noreply.github.com>
2022-06-15 17:21:47 +0900

    Create function.js

---
+++ syntax/function.js
@@ -0,0 +1,20 @@
+function f123() {
+    console.log(1);
+    console.log(2);
+    console.log(3);
+}
+
+f123();
+console.log('----------');
+f123();
+console.log('----------');
+
+console.log(Math.round(1.213323));
+console.log(Math.round(2.5666));
+
+function sum(arg1, arg2) {
+    return arg1 + arg2;
+}
+
+console.log(sum(2, 3));
+
